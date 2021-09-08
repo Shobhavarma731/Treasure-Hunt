b446b24f31e27db942c7418013050eb4514aedee
Shobhavarma731 <60135968+Shobhavarma731@users.noreply.github.com>
2021-09-08 19:32:51 +0530

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "JAI HIND";
+const accessCode2 = "COMMON SENSE";
+const accessCode3 = "WINTER";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("I J A H N I D", 100,50)
+    fill("lightblue")
+    text("Hint: Slogan by Netaji Subhash Chandra Bose.", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("C M M S O O N S E N E", 700,150)
+    fill("lightblue")
+    text("Hint: You must need it.!! ", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("R E W N I T", 100,250)
+    fill("lightblue")
+    text("Hint: Its a season !!", 100,270)
+
+}
